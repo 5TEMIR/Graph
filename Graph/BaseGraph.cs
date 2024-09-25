8e3d1c35c7f7b61cf2437ffb49aba1e71c68df4a
5TEMIR <stemir2015@yandex.ru>
2024-09-25 20:43:29 +0400

    Base graph

---
+++ Graph/BaseGraph.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Graph
+{
+    public class BaseGraph
+    {
+        private Dictionary<string, Dictionary<string, int>>? _adjacensyList;
+        private bool _direct;
+        private bool _measured;
+
+        private static IEnumerable<string> ReadFrom(string file)
+        {
+            string? line;
+            using var reader = File.OpenText(file);
+            while ((line = reader.ReadLine()) != null)
+            {
+                yield return line;
+            }
+        }
+
+        public Dictionary<string, Dictionary<string, int>>? AdjacensyList { get { return _adjacensyList; } }
+        public bool Direct { get { return _direct; } }
+        public bool Measured { get { return _measured; } }
+
+        public BaseGraph()
+        {
+            _adjacensyList = new();
+            _direct = false;
+            _measured = false;
+        }
+
+        public BaseGraph(BaseGraph graph)
+        {
+            _adjacensyList = graph._adjacensyList;
+            _direct = graph._direct;
+            _measured = graph._measured;
+        }
+
+        public BaseGraph(string fileAdjacensyList)
+        {
+            _adjacensyList = new();
+
+            IEnumerable<string> textfile = ReadFrom(fileAdjacensyList);
+            int i = 0;
+            foreach (string line in textfile)
+            {
+                if (i == 0)
+                    _direct = bool.Parse(line);
+                else if (i == 1)
+                    _measured = bool.Parse(line);
+                else
+                {
+                    string[] lineelements = line.Split(" ");
+                    string vertex = lineelements[0];
+                    _adjacensyList.Add(vertex, new Dictionary<string, int>());
+
+                    string adjacensyvertex = "";
+                    for (int j = 1; j < lineelements.Length; j++)
+                    {
+                        if (j % 2 != 0)
+                            adjacensyvertex = lineelements[j];
+                        else
+                            _adjacensyList[vertex][adjacensyvertex] = Int32.Parse(lineelements[j]);
+                    }
+                }
+                i++;
+            }
+        }
+
+        public void OutGraph(string filename)
+        {
+            StreamWriter writer = new StreamWriter(filename, false);
+
+            foreach (var vertex in _adjacensyList)
+            {
+                writer.Write($"{vertex.Key} ");
+                int i = 1;
+                int adjVertexCount = vertex.Value.Count;
+                foreach (var adjacensyvertex in vertex.Value)
+                {
+                    if (i != adjVertexCount)
+                        writer.Write($"{adjacensyvertex.Key} {adjacensyvertex.Value} ");
+                    else
+                        writer.Write($"{adjacensyvertex.Key} {adjacensyvertex.Value}");
+                    i++;
+                }
+                writer.WriteLine();
+            }
+            writer.Close();
+        }
+
+        public void AddVertex(string vertex)
+        {
+            if (!_adjacensyList.ContainsKey(vertex))
+                _adjacensyList.Add(vertex, new Dictionary<string, int>());
+            else
+                Console.WriteLine("Вершина уже существует.");
+        }
+
+        public void RemoveVertex(string vertex)
+        {
+            if (_adjacensyList.ContainsKey(vertex))
+            {
+                foreach (var adjacensyvertex in _adjacensyList)
+                    adjacensyvertex.Value.Remove(vertex);
+                _adjacensyList.Remove(vertex);
+            }
+            else Console.WriteLine("Вершины не существует.");
+        }
+
+        public void AddEdge(string vertex1, string vertex2, int weight)
+        {
+            if (!_adjacensyList.ContainsKey(vertex1) || !_adjacensyList.ContainsKey(vertex2))
+                Console.WriteLine("Вершин не существует.");
+            else
+            {
+                if (!_adjacensyList[vertex1].ContainsKey(vertex2) || !_adjacensyList[vertex2].ContainsKey(vertex1))
+                {
+                    _adjacensyList[vertex1][vertex2] = weight;
+                    if (!_direct)
+                        _adjacensyList[vertex2][vertex1] = weight;
+                }
+                else Console.WriteLine("Ребро уже существует.");
+            }
+        }
+
+        public void RemoveEdge(string vertex1, string vertex2)
+        {
+            if (!_adjacensyList.ContainsKey(vertex1) || !_adjacensyList.ContainsKey(vertex2))
+                Console.WriteLine("Вершин не существует.");
+            else
+            {
+                if (_adjacensyList[vertex1].ContainsKey(vertex2) || _adjacensyList[vertex2].ContainsKey(vertex1))
+                {
+                    _adjacensyList[vertex1].Remove(vertex2);
+                    if (!_direct)
+                        _adjacensyList[vertex2].Remove(vertex1);
+                }
+                else Console.WriteLine("Ребра не существует.");
+            }
+        }
+
+        public void AllNotAdjacencyVertexDirect(string currentvertex)
+        {
+            if (!_direct)
+                Console.WriteLine("Граф не ориентированный.");
+            else
+            {
+                foreach (var vertex in _adjacensyList)
+                {
+                    if (!vertex.Value.ContainsKey(currentvertex) && vertex.Key != currentvertex)
+                        Console.Write($"|{vertex.Key}| ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public void AllNotAdjacencyVertex(string currentvertex)
+        {
+            if (_direct)
+                Console.WriteLine("Граф ориентированный.");
+            else
+            {
+                Dictionary<string, int> adjacencyvertex = _adjacensyList[currentvertex];
+                foreach (var vertex in _adjacensyList)
+                    if (!adjacencyvertex.ContainsKey(vertex.Key) && vertex.Key != currentvertex)
+                        Console.Write($"|{vertex.Key}| ");
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Graph/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Graph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseGraph baseGraph = new BaseGraph("C:\\Users\\stemir\\source\\repos\\Graph\\Graph\\1.txt");
+            BaseGraph baseGraph2 = new BaseGraph("C:\\Users\\stemir\\source\\repos\\Graph\\Graph\\1_2.txt");
+
+            while (true)
+            {
+                Console.Write("Commands: outConsole,\n" +
+                              "          outFile,\n" +
+                              "          addVertex,\n" +
+                              "          addEdge,\n" +
+                              "          removeVertex,\n" +
+                              "          removeEdge\n" +
+                              "          task2\n" +
+                              "          task3\n" +
+                              "\nGraph>");
+                string command = Console.ReadLine();
+
+                if (command == "outConsole")
+                {
+                    Dictionary<string, Dictionary<string, int>> adjList = baseGraph.AdjacensyList;
+
+                    foreach (var vertex in adjList)
+                    {
+                        Console.Write($"{vertex.Key} => ");
+
+                        if (baseGraph.Measured)
+                        {
+                            foreach (var adjvertex in vertex.Value)
+                                Console.Write($"|{adjvertex.Key} ({adjvertex.Value})| ");
+                        }
+                        else
+                        {
+                            foreach (var adjvertex in vertex.Value)
+                                Console.Write($"|{adjvertex.Key}| ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                }
+                else if (command == "addVertex")
+                {
+                    Console.Write("vertex: ");
+                    string vertex = Console.ReadLine();
+                    baseGraph.AddVertex(vertex);
+                    Console.WriteLine();
+                }
+                else if (command == "removeVertex")
+                {
+                    Console.Write("vertex: ");
+                    string vertex = Console.ReadLine();
+                    baseGraph.RemoveVertex(vertex);
+                    Console.WriteLine();
+                }
+                else if (command == "addEdg")
+                {
+                    Console.Write("vertex1: ");
+                    string vertex1 = Console.ReadLine();
+                    Console.Write("vertex2: ");
+                    string vertex2 = Console.ReadLine();
+                    int weight;
+                    if (baseGraph.Measured)
+                    {
+                        Console.Write("weight: ");
+                        weight = Int32.Parse(Console.ReadLine());
+                    }
+                    else weight = 0;
+                    baseGraph.AddEdge(vertex1, vertex2, weight);
+                    Console.WriteLine();
+                }
+                else if (command == "removeEdge")
+                {
+                    Console.Write("vertex1: ");
+                    string vertex1 = Console.ReadLine();
+                    Console.Write("vertex2: ");
+                    string vertex2 = Console.ReadLine();
+                    baseGraph.RemoveEdge(vertex1, vertex2);
+                    Console.WriteLine();
+                }
+                else if (command == "outFile")
+                {
+                    Console.Write("filename: ");
+                    string filename = Console.ReadLine();
+                    baseGraph.OutGraph(filename);
+                    Console.WriteLine();
+                }
+                else if (command == "task2")
+                {
+                    Console.Write("vertex: ");
+                    string vertex = Console.ReadLine();
+                    baseGraph.AllNotAdjacencyVertexDirect(vertex);
+                }
+                else if (command == "task3")
+                {
+                    Console.Write("vertex: ");
+                    string vertex = Console.ReadLine();
+                    baseGraph.AllNotAdjacencyVertex(vertex);
+                }
+            }
+        }
+    }
+}
